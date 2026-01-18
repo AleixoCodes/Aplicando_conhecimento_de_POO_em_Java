80555de3cabd13bad9a5ccff2fa8d4e2d2744ac2
AleixoMartins <aleixomartins2006@gmail.com>
2026-01-17 22:24:09 -0300

    Aperfeiçoando os termos exibidos

--- src/Classe_ManipulaTextoseNumeros/Main.java
+++ src/Classe_ManipulaTextoseNumeros/Main.java
@@ -4,26 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Texto txt = new Texto("Nos Tempos da Brilhantina");
 
-        System.out.println(txt.tamanho());
-        System.out.println(txt.maiusculo());
-        System.out.println(txt.minusculo());
-        System.out.println(txt.subinicio(10));
-        System.out.println(txt.comparador("Programando em Java"));
-        System.out.println(txt.contem("Tempo"));
+        System.out.println("Frase a ser exemplar: " + txt);
+        System.out.println("Tamanho da String: " + txt.tamanho() + " Caracteres");
+        System.out.println("Frase maiuscula: " + txt.maiusculo());
+        System.out.println("Frase minuscula: " + txt.minusculo());
+        System.out.println("Primeiros 10 caracteres: " + txt.subinicio(10));
+        System.out.println("A frase: 'Programando em Java' é " + txt.comparador("Programando em Java") + " que a frase exemplar.");
+        System.out.println("A palavra inserida está contida na frase? " + txt.contem("Tempo"));
         System.out.println(txt.juntar(" com Jhon Travota"));
         System.out.println(txt.localizar("Brilhantina"));
+        System.out.println();
 
-        Numero nums = new Numero(4.20, 8.7);
+        Numero Num = new Numero(4.20, 8.7);
 
-        System.out.println(nums.maior());
-        System.out.println(nums.menor());
-        System.out.println(nums.potencia());
-        System.out.println(nums.raiz());
-        System.out.println(nums.absoluto());
-        System.out.println(nums.ceil());
-        System.out.println(nums.arredondar());
-        System.out.println(nums.aleatorio());
-        System.out.println(nums.seno());
-
+        System.out.println("Números escolhidos: 4.20 e 8.7");
+        System.out.println("O maior: " + Num.maior());
+        System.out.println("O menor: " + Num.menor());
+        System.out.println("Potencia do maior: " + Num.potencia());
+        System.out.println("Raiz do maior: " + Num.raiz());
+        System.out.println("Logaritmo do maior: " + Num.log());
+        System.out.println("Valor absoluto: " + Num.absoluto());
+        System.out.println("Arredondado(para cima): " + Num.ceil());
+        System.out.println("Arredondado(para baixo): " + Num.arredondar());
+        System.out.println("Numero aleatorio entre os dois: " + Num.aleatorio());
+        System.out.println("Seno do primeiro numero: " + Num.seno());
     }
 }
